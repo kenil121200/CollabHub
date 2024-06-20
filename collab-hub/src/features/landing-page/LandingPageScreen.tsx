7b0384eed48ad12b94cef20e6d2944eea1883609
krutik-kulkarni <krutik.kulkarni2000@gmail.com>
2024-06-20 20:49:42 -0300

    wip logo slider

--- collab-hub/src/features/landing-page/LandingPageScreen.tsx
+++ collab-hub/src/features/landing-page/LandingPageScreen.tsx
@@ -1,5 +1,153 @@
+import Landigpage from "../../components/ui-components/landing-page/Landigpage";
+import {
+  Header,
+  InfoContainer,
+  LeftOverlay,
+  // Logo,
+  // LogoContainer,
+  LogoImage,
+  LogosContainer,
+  LogosSlide,
+  Main,
+  RightOverlay,
+  // Slider,
+  // SliderTrack,
+} from "./LandingPageStyles";
+import Typewriter from "typewriter-effect";
+import { useEffect, useRef } from "react";
+
 const LandingPage: React.FC = () => {
-  return <h1>Landing page!</h1>;
+  const sliderTrackRef = useRef(null);
+
+  const logos = [
+    {
+      logo: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png",
+    },
+    {
+      logo: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png",
+    },
+    {
+      logo: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png",
+    },
+    {
+      logo: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png",
+    },
+    {
+      logo: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png",
+    },
+    {
+      logo: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png",
+    },
+    {
+      logo: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png",
+    },
+    {
+      logo: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png",
+    },
+    {
+      logo: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png",
+    },
+    {
+      logo: "https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png",
+    },
+  ];
+  return (
+    <>
+      <Main>
+        <InfoContainer>
+          <Header>
+            <Typewriter
+              options={{
+                strings: [
+                  "Discover open source projects on GitHub that match your skills and interests for meaningful contributions.",
+                  "Connect with like-minded developers to share ideas and work together on impactful open source projects.",
+                  "Utilize advanced algorithms to pair your expertise with projects that need your specific skills.",
+                  "Help project maintainers attract skilled contributors and enhance their project's reach within the open source community.",
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 20, // Decrease the delay between each character
+                deleteSpeed: 0,
+              }}
+            />
+          </Header>
+        </InfoContainer>
+        <Landigpage />
+      </Main>
+      {/* <Slider>
+        <SliderTrack>
+          <LogoContainer>
+            {logos.map((logo) => (
+              <Logo>
+                <img src={logo.logo} height={"100%"} width={"100%"} />
+              </Logo>
+            ))}
+          </LogoContainer>
+
+          <LogoContainer>
+            {logos.map((logo) => (
+              <div className="image">
+                <img src={logo.logo} height={"100%"} width={"100%"} />
+              </div>
+            ))}
+          </LogoContainer>
+        </SliderTrack>
+      </Slider> */}
+
+      <LogosContainer>
+        <LeftOverlay />
+        <RightOverlay />
+        <LogosSlide>
+          {/* Add your logo images here */}
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 1"
+          />
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 2"
+          />
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 2"
+          />
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 2"
+          />
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 2"
+          />
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 2"
+          />
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 2"
+          />
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 2"
+          />
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 2"
+          />
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 2"
+          />
+          <LogoImage
+            src="https://1000logos.net/wp-content/uploads/2020/09/Java-Logo-500x313.png"
+            alt="Logo 2"
+          />
+          {/* ... */}
+        </LogosSlide>
+      </LogosContainer>
+    </>
+  );
 };
 
 export default LandingPage;
--- collab-hub/src/features/landing-page/LandingPageStyles.tsx
+++ collab-hub/src/features/landing-page/LandingPageStyles.tsx
@@ -1,9 +1,160 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const Container = styled.div`
+export const Main = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const InfoContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
+  justify-content: center;
   align-items: center;
-  min-height: 100vh;
+  width: 30%;
+  padding: 12px 12px 12px 12px;
 `;
+export const Header = styled.div`
+  font-family: Poppins, sans-serif;
+  font-weight: 700;
+  font-size: 28px;
+  line-height: 40px;
+  font-feature-settings: "tnum", "lnum", "case", "ss04";
+  color: rgb(37, 37, 37);
+`;
+export const slide = keyframes`
+  from {
+    transform: translateX(0);
+  }
+  to {
+    transform: translateX(-100%);
+  }
+`;
+
+export const LogosContainer = styled.div`
+  overflow: hidden;
+  padding: 60px 0;
+  background: white;
+  white-space: nowrap;
+  position: relative;
+
+  &:hover .logos-slide {
+    animation-play-state: paused;
+  }
+`;
+
+export const GradientOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  width: 250px;
+  height: 100%;
+  content: "";
+  z-index: 2;
+`;
+
+export const LeftOverlay = styled(GradientOverlay)`
+  left: 0;
+  background: linear-gradient(to left, rgba(255, 255, 255, 0), white);
+`;
+
+export const RightOverlay = styled(GradientOverlay)`
+  right: 0;
+  background: linear-gradient(to right, rgba(255, 255, 255, 0), white);
+`;
+
+export const LogosSlide = styled.div`
+  display: inline-block;
+  animation: ${slide} 10s infinite linear;
+`;
+
+export const LogoImage = styled.img`
+  height: 50px;
+  margin: 0 40px;
+`;
+
+// const scroll = keyframes`
+// from {
+//   transform: translateX(0);
+// }
+// to {
+//   transform: translateX(-100%);
+// }
+// `;
+
+// export const Slider = styled.div`
+//   margin-top: 64px;
+//   width: 100%;
+//   height: 120px;
+//   overflow: hidden;
+// `;
+
+// export const SliderTrack = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   display: flex;
+// `;
+
+// export const LogoContainer = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   display: flex;
+//   animation: ${scroll} 7s linear infinite;
+// `;
+
+// export const Logo = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   margin-left: 4px;
+//   border: 1px solid red;
+// `;
+
+// // @keyframes slide {
+// //   from {
+// //     transform: translateX(0);
+// //   }
+// //   to {
+// //     transform: translateX(-100%);
+// //   }
+// // }
+
+// export const logos = styled.div`
+//   overflow: hidden;
+//   padding: 60px 0;
+//   background: white;
+//   white-space: nowrap;
+//   position: relative;
+// `
+
+// .logos:before,
+// .logos:after {
+//   position: absolute;
+//   top: 0;
+//   width: 250px;
+//   height: 100%;
+//   content: "";
+//   z-index: 2;
+// }
+
+// .logos:before {
+//   left: 0;
+//   background: linear-gradient(to left, rgba(255, 255, 255, 0), white);
+// }
+
+// .logos:after {
+//   right: 0;
+//   background: linear-gradient(to right, rgba(255, 255, 255, 0), white);
+// }
+
+// .logos:hover .logos-slide {
+//   animation-play-state: paused;
+// }
+
+// .logos-slide {
+//   display: inline-block;
+//   animation: 35s slide infinite linear;
+// }
+
+// .logos-slide img {
+//   height: 50px;
+//   margin: 0 40px;
+// }
