12ac9ab77adde01cda9ee9a307169b5d663a7c2b
Jainishkumar Bhaveshkumar Patel <jn891368@dal.ca>
2024-06-24 17:17:37 -0300

    Contact us page

--- collab-hub/src/features/ContactUs/ContactUs.tsx
+++ collab-hub/src/features/ContactUs/ContactUs.tsx
@@ -1,7 +1,33 @@
+import { toast } from 'react-toastify';
+import Navbar from '../../components/navbar/Navbar';
+import { Main, LeftTextSection, InfoContainer, Header, Text, Form, Input, TextArea, Button } from './ContactUsStyles';
 const ContactUs: React.FC = () => {
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    toast.success("Thank you for contacting us. We will get back to you as soon as possible.");
+  };
+
   return (
     <>
-      <h1>Contact Us!</h1>
+      <Navbar />
+      <Main>
+        <Header>Need Help?</Header>
+        <div style={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
+          <LeftTextSection>
+            <Text>You can fill out the contact form and CollabHub will get back to you as soon as possible. Thank you for your trust.</Text>
+          </LeftTextSection>
+          <InfoContainer>
+            <Form onSubmit={handleSubmit}>
+              <Input placeholder="Name" />
+              <Input placeholder="Email" required />
+              <Input placeholder="Subject" required />
+              <TextArea placeholder="Message" required />
+              <Button type="submit">Submit</Button>
+            </Form>
+          </InfoContainer>
+        </div>
+      </Main>
     </>
   );
 };
